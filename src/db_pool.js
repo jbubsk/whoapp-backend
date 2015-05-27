4cd120c4724d117f7d8f1611cb1801f12f97a908
Mikhail Zheludev <develojbu@gmail.com>
2015-05-27 12:43:51 +0300

    changed db connection log

--- src/db_pool.js
+++ src/db_pool.js
@@ -1,18 +1,25 @@
 var mysql    = require('mysql'),
     config   = require('./config'),
-    logger = require('./logger-winston'),
+    logger   = require('./logger-winston'),
     database = config.database;
-
-logger.info("Connecting to DB... host: " + database.host[process.env.NODE_ENV]);
 
 var pool = mysql.createPool({
     connectionLimit : database.poolSize,
-    host            : database.host[process.env.NODE_ENV],
+    host            : database.host,
     database        : database.schema,
     user            : database.user,
     password        : database.password,
     debug           : config.debug
 });
+
+var connectionLog = "Connected to DB:\n\t" +
+    "host: " + pool.config.connectionConfig.host +
+    "\n\tport: " + pool.config.connectionConfig.port +
+    "\n\tuser: " + pool.config.connectionConfig.user +
+    "\n\tdatabase: " + pool.config.connectionConfig.database;
+
+logger.info(connectionLog);
+
 
 function getConnection(successCallback, errorCallback) {
 
