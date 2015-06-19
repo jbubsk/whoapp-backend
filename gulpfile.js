7831a22d81c0290b6aa99695a8860a948de02f2b
Mikhail Zheludev <develojbu@gmail.com>
2015-06-19 12:42:20 +0300

    changed build:config to build-confid

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,11 @@ require('./gulp/build')(gulp);
 require('./gulp/run-server')(gulp);
 require('./gulp/test')(gulp);
 
-gulp.task('default', ['build-config'], function () {
+gulp.task('default', function () {
+    console.log("********** Hello from Gulp! **********");
+});
+
+gulp.task('dev', ['build-config'], function () {
     gulp.start('run-server-dev');
 });
 
