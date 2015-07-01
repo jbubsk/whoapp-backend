7014b37c903883a24ea6f64809886a61f98ab3d1
Mikhail Zheludev <develojbu@gmail.com>
2015-07-01 15:45:29 +0300

    changed error handling for authentication route

--- src/routes/auth/logout/index.js
+++ src/routes/auth/logout/index.js
@@ -5,7 +5,7 @@ module.exports = function (req, res, next) {
     if (req.user) {
         authenticationService.logout(req.user.username, function (err, result) {
             if (err) {
-                res.status(err.code).json({message: err.message});
+                res.status(err.status || 400).json({message: err.message, code: err.code});
             } else {
                 req.logout();
                 res.json({
